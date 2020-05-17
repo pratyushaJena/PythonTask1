9f6d318800018b400001bb9ac23582747452aa0c
pratyushaJena <64605826+pratyushaJena@users.noreply.github.com>
2020-05-17 14:21:49 +0530

    save

--- Program8.py
+++ Program8.py
@@ -4,7 +4,7 @@ print('The list of all the columns:')
 for col in ser:
     print(col)
 colname = input('Enter the name of the series from above whose contents you want to see:')
-print('The contents of sries',end=" ")
+print('The contents of series',end=" ")
 print(colname)
 series=pd.Series(ser[colname])
 print(series)
